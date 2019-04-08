c1aba6a21af7fb38ff4d35703810d802491900d8
RhadooPopescu <radu.popesku@gmail.com>
2019-04-08 20:18:45 +0100

    Indent User Class

--- GroupProject/src/User.java
+++ GroupProject/src/User.java
@@ -3,17 +3,17 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class User {
-    private int ID,cardCode;
+    private int ID, cardCode;
     private long cardNo;
-    private String title, fName, lName, address1, address2, town, postcode,username,password;
-    private String email,phoneNo,orgName,webAddress,orgEmail,paymentMethod,userType;
+    private String title, fName, lName, address1, address2, town, postcode, username, password;
+    private String email, phoneNo, orgName, webAddress, orgEmail, paymentMethod, userType;
 
-    public User(){}
+    public User() {
+    }
 
     public User(long cardNo, int cardCode, String title, String fName, String lName, String address1,
                 String address2, String town, String postcode, String username, String password, String email,
-                String phoneNo)
-    {
+                String phoneNo) {
         this.cardNo = cardNo;
         this.cardCode = cardCode;
         this.title = title;
@@ -32,8 +32,7 @@ public class User {
 
     public User(long cardNo, int cardCode, String title, String fName, String lName, String address1,
                 String address2, String town, String postcode, String username, String password, String email,
-                String phoneNo, String orgName, String webAddress, String orgEmail, String paymentMethod)
-    {
+                String phoneNo, String orgName, String webAddress, String orgEmail, String paymentMethod) {
         this.cardNo = cardNo;
         this.cardCode = cardCode;
         this.title = title;
@@ -95,96 +94,107 @@ public class User {
     }
 
     public void insertCustomerData() {
-        String query="";
-        if (this.userType=="customer"){
-            query = "INSERT INTO tbl_user(UserID,Title,Fname,LNAme,Address1,Address2,Town,PostCode,Username,Pass,Email"+
-                ",PhoneNo,CardNo,CVVCode,Type) VALUES(DEFAULT,'"+this.title+"','"+this.fName+"','"+this.lName+"','"
-                +this.address1+"','"+ this.address2+"','"+this.town+"','"+this.postcode+"','"+this.username+"','"
-                +this.password+"','"+this.email+"','"+this.phoneNo+"',"+this.cardNo+","+this.cardCode+",'"+this.userType+
-                "');";}
-        else{
-            query = "INSERT INTO tbl_user(UserID,Title,Fname,LNAme,Address1,Address2,Town,PostCode,Username,Pass,Email"+
+        String query = "";
+        if (this.userType == "customer") {
+            query = "INSERT INTO tbl_user(UserID,Title,Fname,LNAme,Address1,Address2,Town,PostCode,Username,Pass,Email" +
+                    ",PhoneNo,CardNo,CVVCode,Type) VALUES(DEFAULT,'" + this.title + "','" + this.fName + "','" + this.lName + "','"
+                    + this.address1 + "','" + this.address2 + "','" + this.town + "','" + this.postcode + "','" + this.username + "','"
+                    + this.password + "','" + this.email + "','" + this.phoneNo + "'," + this.cardNo + "," + this.cardCode + ",'" + this.userType +
+                    "');";
+        } else {
+            query = "INSERT INTO tbl_user(UserID,Title,Fname,LNAme,Address1,Address2,Town,PostCode,Username,Pass,Email" +
                     ",PhoneNo,CardNo,CVVCode,Type,OrganizationName,WebAddress,OrgEmail,PaymentMethod) VALUES(DEFAULT,'"
-                    +this.title+"','"+this.fName+"','"+this.lName+"','" +this.address1+"','"+ this.address2+
-                    "','"+this.town+"','"+this.postcode+"','"+this.username+"','" +this.password+"','"+this.email+"','"+
-                    this.phoneNo+"',"+this.cardNo+","+this.cardCode+",'"+this.userType+"','"+this.orgName+"','"+
-                    this.webAddress+"','"+this.orgEmail+"','"+this.paymentMethod+"');";}
+                    + this.title + "','" + this.fName + "','" + this.lName + "','" + this.address1 + "','" + this.address2 +
+                    "','" + this.town + "','" + this.postcode + "','" + this.username + "','" + this.password + "','" + this.email + "','" +
+                    this.phoneNo + "'," + this.cardNo + "," + this.cardCode + ",'" + this.userType + "','" + this.orgName + "','" +
+                    this.webAddress + "','" + this.orgEmail + "','" + this.paymentMethod + "');";
+        }
 
-        try{
-            Connect.updateData(query);}
-        catch(SQLException e){e.printStackTrace();}
-        catch(ClassNotFoundException k){
-            System.out.println(k.getMessage());}
+        try {
+            Connect.updateData(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException k) {
+            System.out.println(k.getMessage());
+        }
     }
 
-    public boolean checkFieldInDB(String field, String attribute)
-    {
-        String query = "SELECT * FROM tbl_user WHERE "+field+"='"+attribute+"';";
-        try{
+    public boolean checkFieldInDB(String field, String attribute) {
+        String query = "SELECT * FROM tbl_user WHERE " + field + "='" + attribute + "';";
+        try {
             ResultSet results = Connect.selectStm(query);
-            if(results.next()){return true;} }
-        catch(SQLException e){e.printStackTrace();}
-        catch(ClassNotFoundException k){
-            System.out.println(k.getMessage());}
+            if (results.next()) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException k) {
+            System.out.println(k.getMessage());
+        }
         return false;
     }
 
-    public boolean loginCheck()
-    {
-        String query = "SELECT * FROM tbl_user WHERE Username='"+this.username+"' AND Pass='"+this.password+"';";
-        try{
+    public boolean loginCheck() {
+        String query = "SELECT * FROM tbl_user WHERE Username='" + this.username + "' AND Pass='" + this.password + "';";
+        try {
             ResultSet results = Connect.selectStm(query);
-            if(results.next()){return true;} }
-        catch(SQLException e){e.printStackTrace();}
-        catch(ClassNotFoundException k){
-            System.out.println(k.getMessage());}
+            if (results.next()) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException k) {
+            System.out.println(k.getMessage());
+        }
         return false;
     }
 
-    public static void deleteUser(int ID)
-    {
-        String query = "DELETE FROM tbl_user WHERE ID='"+ID+"';";
-        try{
-            Connect.updateData(query);}
-        catch(SQLException e){e.printStackTrace();}
-        catch(ClassNotFoundException k){
-            System.out.println(k.getMessage());}
+    public static void deleteUser(int ID) {
+        String query = "DELETE FROM tbl_user WHERE ID='" + ID + "';";
+        try {
+            Connect.updateData(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException k) {
+            System.out.println(k.getMessage());
+        }
     }
 
-    public static ArrayList userList()
-    {
+    public static ArrayList userList() {
         String query = "SELECT Username FROM tbl_user;";
         ArrayList<String> users = new ArrayList<>();
-        try{
+        try {
             ResultSet results = Connect.selectStm(query);
-            while(results.next())
-            {
+            while (results.next()) {
                 String user = results.getString("Username");
                 users.add(user);
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException k) {
+            System.out.println(k.getMessage());
         }
-        catch(SQLException e){e.printStackTrace();}
-        catch (ClassNotFoundException k){
-            System.out.println(k.getMessage());}
         return users;
     }
 
-    public String getType(String username)
-    {
-     String query = "SELECT Type FROM tbl_user WHERE Username='"+username+"';";
-     String type = "";
-     try{
-        ResultSet rs = Connect.selectStm(query);
-        rs.next();
-        type = rs.getString("Type");}
-     catch(SQLException e){e.printStackTrace();}
-     catch (ClassNotFoundException f){
-         System.out.println(f.getMessage());}
-     return type;
+    public String getType(String username) {
+        String query = "SELECT Type FROM tbl_user WHERE Username='" + username + "';";
+        String type = "";
+        try {
+            ResultSet rs = Connect.selectStm(query);
+            rs.next();
+            type = rs.getString("Type");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException f) {
+            System.out.println(f.getMessage());
+        }
+        return type;
 
 
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         ArrayList<String> test = userList();
 
         System.out.println(test.get(0));
